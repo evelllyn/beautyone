ffaede75b6c276fb6cb000857d8729a66ecef640
evelllyn <tinghsin027@gmail.com>
2023-10-10 16:47:13 +0800

    v1.0

---
+++ src/components/DelModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" ref="modal">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title">
+            <span>刪除{{ item.title }}</span>
+          </h5>
+          <button type="button" class="btn-close"
+                  data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          是否刪除<strong class="text-danger">{{ item.title }}</strong>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary"
+                  data-bs-dismiss="modal">取消
+          </button>
+          <button type="button" class="btn btn-primary" @click="$emit('del-item')">確認</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modalMixin'
+
+export default {
+  props: {
+    item: {}
+  },
+  data () {
+    return {
+      modal: {}
+    }
+  },
+  mixins: [modalMixin]
+}
+</script>
